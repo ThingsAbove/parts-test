3038998e0b2e72f8d9526d50777b035d0a0e7e27
ThingsAbove <richard.c.carpenter@gmail.com>
2014-04-03 13:59:15 -0400

    change Bin admin view

--- inventory/admin.py
+++ inventory/admin.py
@@ -7,13 +7,17 @@ class PartInline(admin.TabularInline):
     model = Part
     extra = 3
     
+class PartAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    
 class SupplierAdmin(admin.ModelAdmin):
     fields = ['name', 'contact']
     inlines = [PartInline]    
     
 class BinAdmin(admin.ModelAdmin):
     fields = ['part_type','capacity','count','location','replenish_date']
+    list_display = ('part_type','capacity','count','location','replenish_date')
 
-admin.site.register(Part)
+admin.site.register(Part, PartAdmin)
 admin.site.register(Bin, BinAdmin)
 admin.site.register(Supplier, SupplierAdmin)
