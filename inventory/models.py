268602515719b9f58ecb4891182b57587d3f585b
ThingsAbove <richard.c.carpenter@gmail.com>
2014-04-03 10:28:25 -0400

    Update model to report object type better

--- inventory/models.py
+++ inventory/models.py
@@ -3,14 +3,20 @@ from django.db import models
 # Create your models here.
     
 class Supplier(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.name    
     name = models.CharField(max_length=200)
 
 class Part(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.name      
     supplier = models.ForeignKey(Supplier)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     
 class Bin(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.location    
     part_type = models.ForeignKey(Part)
     capacity = models.IntegerField(default=100)
     count = models.IntegerField(default=0)
