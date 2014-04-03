de2dd064992f7acdebd84b5ff245837701086e53
ThingsAbove <richard.c.carpenter@gmail.com>
2014-04-03 13:54:32 -0400

    change Bin admin view

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,14 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from inventory.models import Supplier,Part,Bin
 
-class PartInline(admin.StackedInline):
+class PartInline(admin.TabularInline):
     model = Part
     extra = 3
     
 class SupplierAdmin(admin.ModelAdmin):
     fields = ['name', 'contact']
     inlines = [PartInline]    
+    
+class BinAdmin(admin.ModelAdmin):
+    fields = ['part_type','capacity','count','location','replenish_date']
 
 admin.site.register(Part)
-admin.site.register(Bin)
+admin.site.register(Bin, BinAdmin)
 admin.site.register(Supplier, SupplierAdmin)
