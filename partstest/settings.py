5e5ab7fe406f2e76fc3cf507a77b0071170cea17
ThingsAbove <richard.c.carpenter@gmail.com>
2014-04-03 13:42:13 -0400

    Update column on supplier

--- partstest/settings.py
+++ partstest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
+#    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,10 +58,18 @@ WSGI_APPLICATION = 'partstest.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+     'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'inventory',
+        'USER': 'chris',
+        'PASSWORD': 'bluesky73',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+   }
 }
 
 # Internationalization
