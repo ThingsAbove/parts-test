434c107a6fee5529a2a9d7d32ae240d962a6758e
Richard Carpenter <richard.c.carpenter@gmail.com>
2014-04-16 17:01:07 -0400

    changed models added cost, class attributes and added Warehouse model-- db changes required"

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,14 +3,14 @@ import moneyed
 from djmoney.models.fields import MoneyField
 
 # Register your models here.
-from inventory.models import Supplier,Part,Bin
+from inventory.models import Supplier,Part,Bin,Warehouse
 
 class PartInline(admin.TabularInline):
     model = Part
     extra = 3
     
 class PartAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'cost',)
+    list_display = ('name', 'description', 'cost', 'part_class',)
       
 class SupplierAdmin(admin.ModelAdmin):
     fields = ['name', 'contact']
@@ -20,8 +20,17 @@ class BinAdmin(admin.ModelAdmin):
     fields = ['part_type','capacity','count','location','replenish_date']
     list_display = ('part_type', 'capacity','count','cost','percent_remaining', 'location', 'replenish_date', 'days_since_replenished',)
     list_filter = ['replenish_date']
+    
+class BinInline(admin.TabularInline):
+	model=Warehouse.bins.through
+	extra =3
+	
+class WarehouseAdmin(admin.ModelAdmin):
+	fields = ('name',)
+	inlines = [BinInline]
 
 admin.site.register(Part, PartAdmin)
 admin.site.register(Bin, BinAdmin)
 admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Warehouse,WarehouseAdmin)
 
--- inventory/models.py
+++ inventory/models.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 import moneyed
 from djmoney.models.fields import MoneyField
 from moneyed import Money, USD
+from django.db.models import Count, Max, Avg, Sum
 
 # Create your models here.
 SECS_IN_DAY = 86400 # seconds in a day
@@ -16,15 +17,26 @@ class Supplier(models.Model):
 
 class Part(models.Model):
     def __unicode__(self):  # Python 3: def __str__(self):
-        return self.name      
+        return (self.name + ':class:' + self.part_class +':cost-per:' + str(self.cost))     
     supplier = models.ForeignKey(Supplier)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     cost = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    PART_CLASS_A = 'A'
+    PART_CLASS_B = 'B'
+    PART_CLASS_C = 'C'
+    PART_CLASS_UNKNOWN = 'U'
+    PART_CLASS_CHOICES=(
+        (PART_CLASS_A, 'very tight control and accurate records'),
+        (PART_CLASS_B, 'less tightly controlled and good records'),
+        (PART_CLASS_C, 'simplest controls possible and minimal records'),
+        (PART_CLASS_UNKNOWN, 'not applicable or unknown'),
+    )
+    part_class = models.CharField(max_length=1, choices=PART_CLASS_CHOICES, default=PART_CLASS_C)
 
 class Bin(models.Model):
     def __unicode__(self):  # Python 3: def __str__(self):
-        return self.location    
+        return (self.location + ':' + str(self.part_type))
     part_type = models.ForeignKey(Part)
     capacity = models.IntegerField(default=100)
     count = models.IntegerField(default=0)
@@ -37,6 +49,12 @@ class Bin(models.Model):
         else:
             return (Money(0,'USD'))
     
+    def contains_part_class_category(self):
+        if self.part_type:
+            return (self.part_type.part_class)
+        else:
+            return Part.PART_CLASS_UNKNOWN
+            
     def days_since_replenished(self):
         return int(((timezone.now() - self.replenish_date).total_seconds())/SECS_IN_DAY)
 
@@ -55,3 +73,33 @@ class Bin(models.Model):
         if self.replenish_date > timezone.now():
             raise ValidationError('Replenish date cannot be in the future')
 
+class Warehouse(models.Model):
+    def __unicode__(self):
+            return self.name
+    name = models.CharField(max_length=200)
+    bins = models.ManyToManyField(Bin)
+    def number_of_bins(self):
+        if bins:
+            return bins.objects.count()
+        else:
+            return 0
+    def number_of_part_class_a_bins(self):
+        if bins:
+            return (bins.objects.filter(bins__part_type__part_class=Part.PART_CLASS_A).count())
+        else:
+            return 0
+    def number_of_part_class_b_bins(self):
+        if bins:
+            return (bins.objects.filter(bins__part_type__part_class=Part.PART_CLASS_B).count())
+        else:
+            return 0
+    def number_of_part_class_c_bins(self):
+        if bins:
+            return (bins.objects.filter(bins__part_type__part_class=Part.PART_CLASS_C).count())
+        else:
+            return 0
+    def total_cost(self):
+        if bins:
+            return (bins.objects.aggregate(Sum(part_type.cost)))
+        else:
+            return Money(0,'USD')
